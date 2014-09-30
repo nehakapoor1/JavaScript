e799fb802e5f7ec9d47ee359e95cba0eaeea6713
nehakapoor1 <nehakapahi@gmail.com>
2014-09-30 12:58:21 +0530

    Create pubsub.js

---
+++ pubsub.js
@@ -0,0 +1,27 @@
+define(function() {
+	var cache = {};
+
+	return {
+	pub: function (id) {
+		var args = [].slice.call(arguments,1);
+
+		if(!cache[id]) {
+		cache[id] = [];
+		}
+
+		for (var i = 0,il = cache[id].length;i < il; i++) {
+			cache[id][i].apply(null,args);
+		}
+	},
+
+	sub: function (id, fn) {
+
+		if(!cache[id]) {
+		cache[id] = [fn];
+		}
+		else{
+		cache[id].push(fn);
+		}
+	}
+	};
+});
