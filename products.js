6285a406e02a7c409f103f4082c4fc429c1c93e3
nehakapoor1 <nehakapahi@gmail.com>
2014-09-30 13:18:31 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,4 +1,4 @@
-require(['./product-list.js', './big-cart.js', './mini-cart.js' ], function (productList, bigCart, miniCart) {
+require(['product-list.js', 'big-cart.js', 'mini-cart.js' ], function (productList, bigCart, miniCart) {
 		$(document).ready(function () {
 			bigCart.init();
 			miniCart.init();
