631f7c8313de07656520be9608fccad58dc2d34e
nehakapoor1 <nehakapahi@gmail.com>
2014-09-30 11:24:27 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,7 @@
+require(['./product-list.js', './big-cart.js', './mini-cart.js' ], function (productList, bigCart, miniCart) {
+		$(document).ready(function () {
+			bigCart.init();
+			miniCart.init();
+			productList.init();
+		});
+});
